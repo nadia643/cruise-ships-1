186252d10135b6d6ccf0607ea7dbf6b480bc2616
joestephens <joe.stephens@live.co.uk>
2018-03-05 01:09:41 +0000

    Port test and constructor

---
+++ __tests__/Port.test.js
@@ -0,0 +1,14 @@
+/* globals describe it expect */
+const Port = require('../src/Port.js');
+
+describe('Port', () => {
+  it('can be instantiated', () => {
+    expect(new Port()).toBeInstanceOf(Object);
+  });
+
+  it('has a name', () => {
+    const port = new Port('Dover');
+
+    expect(port.name).toBe('Dover');
+  });
+});
