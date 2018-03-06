31f01915c62eddf673b547337aa861aaf85c0504
Joe Stephens <joe@webantic.co.uk>
2018-03-06 14:09:24 +0000

    Refactor Ship test suite

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,6 +1,7 @@
 /* globals describe it expect */
 const Ship = require('../src/Ship.js');
 const Port = require('../src/Port.js');
+const Itinerary = require('../src/Itinerary.js');
 
 describe('Ship', () => {
   it('can be instantiated', () => {
@@ -9,14 +10,16 @@ describe('Ship', () => {
 
   it('has a starting port', () => {
     const port = new Port('Dover');
-    const ship = new Ship(port);
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
 
     expect(ship.currentPort).toBe(port);
   });
 
   it('can set sail', () => {
     const port = new Port('Dover');
-    const ship = new Ship(port);
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
 
     ship.setSail();
 
@@ -25,10 +28,11 @@ describe('Ship', () => {
 
   it('can dock at a different port', () => {
     const dover = new Port('Dover');
-    const ship = new Ship(dover);
-
     const calais = new Port('Calais');
-    ship.dock(calais);
+    const itinerary = new Itinerary([dover, calais]);
+    const ship = new Ship(itinerary);
+
+    ship.dock();
 
     expect(ship.currentPort).toBe(calais);
   });
