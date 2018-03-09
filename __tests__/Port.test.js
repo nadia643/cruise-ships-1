b7565725b8815ba06dae9c5ababc3682291a8837
joestephens <joe.stephens@live.co.uk>
2018-03-09 13:30:06 +0000

    port ships wip

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,9 +1,34 @@
 /* globals describe it expect */
 const Port = require('../src/Port.js');
+const Ship = require('../src/Ship.js');
+const Itinerary = require('../src/Itinerary.js');
 
 describe('Port', () => {
   it('can be instantiated', () => {
     expect(new Port()).toBeInstanceOf(Object);
+  });
+
+  it('can add a ship', () => {
+    const port = new Port('Dover');
+    const itinerary = new Itinerary([port]);
+    const ship = new Ship(itinerary);
+
+    port.addShip(ship);
+
+    expect(port.ships).toContain(ship);
+  });
+
+  it('can remove a ship', () => {
+    const port = new Port('Dover');
+    const itinerary = new Itinerary([port]);
+    const titanic = new Ship(itinerary);
+    const queenMary = new Ship(itinerary);
+
+    port.addShip(titanic);
+    port.addShip(queenMary);
+    port.removeShip(queenMary);
+
+    expect(port.ships).toEqual([titanic]);
   });
 
   it('has a name', () => {
