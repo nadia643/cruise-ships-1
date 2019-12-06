f449bd87b462b120d01e277de25c58607337fb81
John Doe <nadia643@gmail.com>
2019-12-06 13:27:08 +0000

    ship created

--- src/controller.js
+++ src/controller.js
@@ -32,6 +32,14 @@
       });
     },
 
+    renderShip: function (ship) {
+      const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+      const shipElement = document.querySelector('#ship');
+      shipElement.style.top = `${portElement.offsetTop + 32}px`;
+      shipElement.style.left = `${portElement.offsetLeft + 32}px`;
+    },
+
   };
 
   if (typeof module !== 'undefined' && module.exports) {
